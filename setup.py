17fe07dd9a4d56a628f41ecdd327f9fa33e9e703
goroyabu <goro.yabu@ipmu.jp>
2020-07-21 13:56:13 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -53,8 +53,8 @@ class CMakeBuild(build_ext):
                                                               self.distribution.get_version())
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        #subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '-G', 'Ninja', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        #subprocess.check_call(['cmake', '-G', 'Ninja', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
         subprocess.check_call(['cmake', '--install', '.'], cwd=self.build_temp)
 
