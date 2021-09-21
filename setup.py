44ae2a6a4adfe347a7ae899c849936ae6fe7caf3
goroyabu <goro.yabu@ipmu.jp>
2021-09-21 13:45:27 +0900

    "merge v2"

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='sim4py',
-    version='2.0.0',
+    version='20210921',
     author='Goro Yabu',
     author_email='goro.yabu@ipmu.jp',
     description='Monte-Carlo Simulator',
